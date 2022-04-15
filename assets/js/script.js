81e9d42558b4455f97a2323db50cd0bc7d4f686a
Roy4th <97925568+Roy4th@users.noreply.github.com>
2022-04-15 18:14:24 -0500

    added color to timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
 var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 
 document.getElementById('currentDay').append(currentDate);
+
+timeCheck = function() {
+    const currentHour = moment().hours()
+    $(".time-block").each(function() {
+        const eventHour = $(this).attr('id')
+        if (eventHour < currentHour) {
+            $(this).addClass('past')
+        } else if (eventHour === currentHour) {
+            $(this).addClass('present')
+            $(this).removeClass('past')
+        } else {
+            $(this).removeClass('past')
+            $(this).removeClass('present')
+            $(this).addClass('future')
+        }
+    })
+}
+
+timeCheck()
